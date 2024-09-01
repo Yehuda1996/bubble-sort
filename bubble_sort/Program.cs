059b5c1d1ebfa73f7eb6206b9a46e7807583cfb3
Yehuda1996 <ygrajower@gmail.com>
2024-09-01 10:48:44 +0300

    selection sort

--- bubble_sort/Program.cs
+++ bubble_sort/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace bubble_sort
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace bubble_sort
 {
 	internal class Program
 	{
@@ -18,6 +20,38 @@
 			}
 			return a;
 		}
+
+		public int[] selection_sort(int[] a)
+		{
+			int length = a.Length;
+			for (int i = 0; i < length; i++)
+			{
+				int min_index = find_min_index(a, i);
+				a = swap(a, i, min_index);	
+			}
+			return a;
+		}
+
+		public int find_min_index(int[] a, int i)
+		{
+			int min_index = i;
+			for (int j = i; j < a.Length; j++)
+			{
+				if (a [j] < a[min_index])
+				{
+					min_index = j;
+				}
+			}
+			return min_index;
+		}
+
+		public int[] swap(int[] a, int i, int j)
+		{
+			int temp = a [i];
+			a [i] = a [j];
+			a [j] = temp;
+			return a;
+		}	
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello, World!");
