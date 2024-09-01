79b61d555f3f9cf978c0413c9ca6afa77ef4df65
Yehuda1996 <ygrajower@gmail.com>
2024-09-01 10:21:58 +0300

    initial commit for bubble sort

---
+++ bubble_sort/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace bubble_sort
+{
+	internal class Program
+	{
+		public int[]  bubble_sort(int[] a)
+		{
+			for (int i = 0; i < a.Length; i++)
+			{
+				for (int j = 0; j < a.Length; j++)
+				{
+					if (a[i] == a[j])
+					{
+						return a;
+					}
+					if (a[i] > a[j])
+					{
+						int temp = a[i];
+						a[i] = a[j];
+						a[j] = temp;
+					}
+				}
+			}
+			return a;
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
